1a3def41e03f8b04617afbb377c637404112a26f
Denver <denver.pierce@s5stratos.com>
2020-11-28 09:49:15 -0600

    Update message output text

--- src/calcs.ts
+++ src/calcs.ts
@@ -6,6 +6,8 @@ export const calculateStats = (
   parsedAnimal: ParsedLine<string>,
   parsedMiles: ParsedLine<string>,
 ): string => {
+  // TODO: this func should just calculate the stat miles
+  // and messaging should be moved somewhere else
   // TODO tuple or w/e
   logger.info(`Doggo parse: ${JSON.stringify(parsedAnimal)}`);
   logger.info(`Miles parse: ${JSON.stringify(parsedMiles)}`);
@@ -13,12 +15,12 @@ export const calculateStats = (
     typeof parsedAnimal.parsedValue === 'number'
     && typeof parsedMiles.parsedValue === 'number'
   ) {
-    const doggos = parsedAnimal.parsedValue;
+    const animals = parsedAnimal.parsedValue;
     const miles = parsedMiles.parsedValue;
-    const doggoMiles = doggos / miles;
-    if (Number.isFinite(doggoMiles)) {
-      return `You went ${doggoMiles}! Amazing!`;
+    const animalMiles = animals / miles;
+    if (Number.isFinite(animalMiles)) {
+      return `You went ${animalMiles} ${parsedAnimal.stat} miles! Amazing!`;
     }
   }
-  return "I couldnt' figure out how many doggo miles you did :(";
+  return `I couldn't figure out how many ${parsedAnimal.stat} miles you did :(`;
 };
