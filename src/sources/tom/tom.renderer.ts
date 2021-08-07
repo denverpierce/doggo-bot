650b99f6f2429d1af438ecbc51f72eac83b85b64
denverpierce <denver.pierce@gmail.com>
2021-08-07 11:58:12 -0500

    Add index filter, more indicies, and remove some logs

--- src/sources/tom/tom.renderer.ts
+++ src/sources/tom/tom.renderer.ts
@@ -1,20 +1,31 @@
 import type { SectionBlock } from '@slack/web-api';
 import logger from '../../logging';
+import { getTypedKeys } from '../../utils';
 import { pollenIntro } from '../amb/amb.renderer';
 import { TomData } from './tom.service';
 
-const pollenIndexToBlock = (
-  treeIndex: number,
+type TomPollenIndicies = TomData["timelines"][0]["intervals"][0]["values"]
+
+const tomPollenIndexToLabelMap = {
+  [0]: 'None',
+  [1]: 'Very low',
+  [2]: 'Low',
+  [3]: 'Medium',
+  [4]: 'High',
+  [5]: 'Very High'
+}
+
+const pollenRiskToBlock = (
+  currentKey: keyof TomPollenIndicies,
+  pollenIndicies: TomPollenIndicies,
 ): SectionBlock => ({
   type: 'section',
   text: {
     type: 'mrkdwn',
-    // TODO: make this run against multiple metrics
-    text: `*Tree Pollen* is currently *${treeIndex}*`,
+    text: `Risk of *${currentKey.replace('Index', ' Pollen').toLocaleUpperCase()}* is *${tomPollenIndexToLabelMap[pollenIndicies[currentKey]]}*`,
   },
 });
 
-/* eslint-disable import/prefer-default-export */
 export const tomPollenToSlack = (
   pollenData: TomData,
 ): SectionBlock[] | undefined => {
@@ -22,8 +33,11 @@ export const tomPollenToSlack = (
   const pollenBlocks:SectionBlock[] = pollenData.timelines
     .flatMap(tms => tms.intervals)
     .flatMap(i => i.values)
-    .flatMap(v => v.treeIndex)
-    .map(pollenIndexToBlock);
+    .flatMap(pollenIndicies => {
+      const riskyPollenKeys = getTypedKeys(pollenIndicies)
+        .filter(risk => pollenIndicies[risk] >= 2); // only build/send messages if pollen index is equal or above "Low"
+        return riskyPollenKeys.map(riskyPollenKey => pollenRiskToBlock(riskyPollenKey, pollenIndicies));
+    })
   if (pollenBlocks.length === 0) return undefined;
   return [pollenIntro, ...pollenBlocks];
 };
--- src/sources/tom/tom.service.ts
+++ src/sources/tom/tom.service.ts
@@ -4,9 +4,12 @@ import { z } from 'zod';
 import logger from '../../logging';
 import { AmbLocation } from '../amb/amb.service';
 
+const tomIndexValues = z.union([z.literal(0),z.literal(1),z.literal(2),z.literal(3),z.literal(4),z.literal(5)])
+
 const tomValue = z.object({
-  // TODO: other metrics
-  treeIndex: z.number(),
+  treeIndex: tomIndexValues,
+  weedIndex: tomIndexValues,
+  grassIndex: tomIndexValues
 });
 const tomInterval = z.object({
   startTime: z.string(),
@@ -32,11 +35,10 @@ export const TomClient = (token: string) => {
 
   return {
     getLatestPollen(location: AmbLocation) {
-      const url = `${TOM_API}/v4/timelines?location=${location.lat},${location.lng}&units=metric&timesteps=current&fields=treeIndex&apikey=${token}`
-      logger.info(`Url is: ${url}`)
+      const url = `${TOM_API}/v4/timelines?location=${location.lat},${location.lng}&units=metric&timesteps=current&fields=treeIndex,grassIndex,weedIndex&apikey=${token}`
+      // logger.info(`Url is: ${url}`)
       return httpClient.get<unknown>(url)
         .then(r => {
-          logger.info(JSON.stringify(r.data))
           const maybeParsedData = tomData.safeParse(r.data);
           if (!maybeParsedData.success) {
             logger.error(`Tom Pollen parsing error: ${JSON.stringify(maybeParsedData.error)}`)
