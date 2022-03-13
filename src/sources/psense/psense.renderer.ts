079d5055e41174b1b0e91fa7355df16014987310
denverpierce <denver.pierce@gmail.com>
2022-03-13 16:23:34 -0500

    Filter pSense response messages

--- src/sources/psense/psense.renderer.ts
+++ src/sources/psense/psense.renderer.ts
@@ -12,6 +12,7 @@ const pSensePollenCategoryToLabel = {
   WEE: 'Weed Particles',
 };
 const miseryIndexToLabel = (misery: number): string => {
+  // SPIKE: may need to decrease the lower bound if it isn't low enough for some users
   if (misery >= 66) {
     return 'High';
   }
@@ -37,7 +38,9 @@ export const pSensePollenToSlack = (
   pollenData: tPSenseData,
 ): SectionBlock[] | undefined => {
   logger.info(`Pollen data received, attempting to build blocks for: ${JSON.stringify(pollenData)}`);
-  const pollenBlocks:SectionBlock[] = pollenData.Categories.map(pollenCategoryToBlock);
+  const pollenBlocks: SectionBlock[] = pollenData.Categories
+    .filter(cat => cat.MiseryHighs[0] > 33) // filter out low High counts so as not to spam the channel all the time
+    .map(pollenCategoryToBlock);
   if (pollenBlocks.length === 0) return undefined;
   return [pollenIntro, ...pollenBlocks];
 };
